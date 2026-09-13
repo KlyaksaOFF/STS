ce789c557b99cc6a33b51745be10d49beb682268
Alex <klyaksaoff@gmail.com>
2026-09-13 12:30:41 +0300

    updated

--- handlers/answer.py
+++ handlers/answer.py
@@ -4,7 +4,7 @@ from aiogram import F, Router, types
 from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from aiogram.types import CallbackQuery
-
+from database.repositories.shop import product_category_list
 router = Router()
 
 
@@ -20,15 +20,21 @@ async def menu(message: types.Message, state: FSMContext):
 
 
 @router.callback_query(F.data == 'category')
-async def category(callback: CallbackQuery):
-    return await callback.message.answer('this is button - category')  # category take with your database in .env
+async def category(callback: CallbackQuery, state: FSMContext):
+    await state.clear()
+    category = await product_category_list()
+    buttons = []
+    keyboard = types.InlineKeyboardMarkup(inline_keyboard=buttons)
+    return await callback.message.answer('this is button - category', reply_markup=keyboard)  # category take with your database in .env
 
 
 @router.callback_query(F.data == 'information')
-async def information(callback: CallbackQuery):
+async def information(callback: CallbackQuery, state: FSMContext):
+    await state.clear()
     return await callback.message.answer('this is button - information')  # write information for your bot
 
 
 @router.message()
-async def unknow_message(message: Message):
+async def unknow_message(message: Message, state: FSMContext):
+    await state.clear()
     return await message.answer(f"I don't know this message '{message.text}', open menu - /menu")
